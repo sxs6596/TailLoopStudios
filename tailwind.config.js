27b0348fe8fcaa0f673833c16aef60255dc0fc4b
sxs6596 <sxs6596@mavs.uta.edu>
2024-02-11 19:35:49 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+    screens:{
+      'sm': '480px',
+      'md': '768px',
+      'lg': '976px',
+      'xl': '1440px',
+    }
+  },
+  plugins: [],
+}
